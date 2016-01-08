35fe1b8b7df70b2f35db2590c1020480b90b791b
Emily Richardson <richarel@rose-hulman.edu>
2016-01-07 19:12:59 -0500

    Added test classes

---
+++ src/test/ClassDeclarationVisitorTest.java
@@ -0,0 +1,9 @@
+package test;
+
+public class ClassDeclarationVisitorTest {
+
+	public ClassDeclarationVisitorTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/test/ClassFieldVisitorTest.java
@@ -0,0 +1,9 @@
+package test;
+
+public class ClassFieldVisitorTest {
+
+	public ClassFieldVisitorTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
