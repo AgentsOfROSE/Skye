99aea61a7eae148a4286188773257913cd9378e9
Emily Richardson <richarel@rose-hulman.edu>
2016-01-13 18:51:07 -0500

    started sequence parser

--- src/umlParser/SequenceDiagramTextParser.java
+++ src/umlParser/SequenceDiagramTextParser.java
@@ -1,12 +1,19 @@
 package umlParser;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
-public class SequenceDiagramTextParser implements Parsable{
+public class SequenceDiagramTextParser implements Parsable {
 
 	@Override
 	public void parse(String[] args) throws IOException {
-		
+		String className = args[0];
+		String methodName = args[1];
+		String[] params = args[2].substring(1, args[2].length()).split(" ");
+		int maxDepth = 5;
+		if (args.length == 4) {
+			maxDepth = Integer.parseInt(args[3]);
+		}
 	}
 
 }
