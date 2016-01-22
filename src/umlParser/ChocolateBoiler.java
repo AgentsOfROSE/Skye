d2325e8602c56d6a533c12f9bf480fcb2fb4d0ef
Trevor Burch <burchtm@rose-hulman.edu>
2016-01-21 22:06:04 -0500

    Added Chocolate Boiler UML

--- src/umlParser/ChocolateBoiler.java
+++ src/umlParser/ChocolateBoiler.java
@@ -3,6 +3,7 @@ package umlParser;
 public class ChocolateBoiler {
 	private boolean empty;
 	private boolean boiled;
+	
 	private static ChocolateBoiler uniqueInstance;
   
 	private ChocolateBoiler() {
@@ -12,10 +13,8 @@ public class ChocolateBoiler {
   
 	public static ChocolateBoiler getInstance() {
 		if (uniqueInstance == null) {
-			System.out.println("Creating unique instance of Chocolate Boiler");
 			uniqueInstance = new ChocolateBoiler();
 		}
-		System.out.println("Returning instance of Chocolate Boiler");
 		return uniqueInstance;
 	}
 
